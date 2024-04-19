4e892fe7f5aed5b0f32064e79a4433089c0452fe
Tazdingo <taste_my_blade@outlook.com>
2024-04-19 21:44:48 +0800

    使用memo函数

--- components/home/Main/index.tsx
+++ components/home/Main/index.tsx
@@ -1,13 +1,17 @@
 import ChatInput from "./ChatInput";
 import Menu from "./Menu";
+import MessageList from "./MessageList";
 import Welcome from "./Welcome";
+
+
 
 export default function Main() {
   return (
     <div className="relative flex-1">
       <main className="overflow-y-auto p-2 w-full h-full bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100">
         <Menu />
-        <Welcome />
+        {/* <Welcome /> */}
+        <MessageList />
         <ChatInput />
       </main>
     </div>
