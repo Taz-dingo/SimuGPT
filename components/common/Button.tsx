59a575fca2d67b204a06ad9442dc9e9a033912dc
Tazdingo <taste_my_blade@outlook.com>
2024-04-19 21:16:07 +0800

    开发消息输入框

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -4,7 +4,7 @@ import { IconType } from "react-icons";
 // 引入icon和样式，重新定义Button类型
 type ButtonProps = {
   icon?: IconType; //（可选）icon
-  variant?: "default" | "outline" | "text"; // （可选）样式属性
+  variant?: "default" | "outline" | "text" | "primary"; // （可选）样式属性
 } & ComponentPropsWithoutRef<"button">; // 继承React.ComponentPropsWithoutRef<"button">类型
 
 export default function Button({
@@ -23,6 +23,8 @@ export default function Button({
           ? "text-black dark:text-gray-300 bg-gray-50 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-900"
           : variant === "outline"
           ? "border border-gray-300 dark:border-gray-600 text-black dark:text-gray-300 bg-gray-50 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+          : variant === 'primary' 
+          ? "bg-primary-500 text-white hover:bg-primary-600 hover:text-white shadow-sm"
           : "text-black dark:text-gray-300 bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700"
       }
       ${className}`}
--- components/home/Main/index.tsx
+++ components/home/Main/index.tsx
@@ -1,11 +1,15 @@
+import ChatInput from "./ChatInput";
 import Menu from "./Menu";
 import Welcome from "./Welcome";
 
 export default function Main() {
   return (
-    <main className="relative overflow-y-auto p-2 flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100">
-      <Menu />
-      <Welcome />
-    </main>
+    <div className="relative flex-1">
+      <main className="overflow-y-auto p-2 w-full h-full bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100">
+        <Menu />
+        <Welcome />
+        <ChatInput />
+      </main>
+    </div>
   );
 }
