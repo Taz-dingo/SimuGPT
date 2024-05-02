0f367b9e3c1cea5d211ddec41883264bb926ec12
Tazdingo <taste_my_blade@outlook.com>
2024-05-02 21:24:20 +0800

    1. 调用OpenAI API前过滤一下message的属性，避免'Additional properties are not allowed'报错 2. 添加侧边栏更新标题功能

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,10 +8,11 @@ export async function POST(request: NextRequest) {
     const encoder = new TextEncoder();
     const stream = new ReadableStream({
         async start(controller) {
-
+            // 过滤掉无用属性，只保留content和role，否则会报错 (OpenAI API)
+            const newMessages = messages.map(message => ({ role: message.role, content: message.content }));
             const events = await client.streamChatCompletions(model,
                 [{ role: "system", content: "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown." },
-                ...messages],
+                ...newMessages],
                 { maxTokens: 1024 });
             for await (const event of events) {
                 for (const choice of event.choices) {
